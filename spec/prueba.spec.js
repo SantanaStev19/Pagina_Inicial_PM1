50c331179e587a2c0b4b67a0145de6ea047b66d0
Steven Santana <steven.santana.s19@gmail.com>
2025-04-19 14:42:15 -0500

    Proyecto Modulo 1

--- spec/prueba.spec.js
+++ spec/prueba.spec.js
@@ -1,5 +1,11 @@
-describe("demo", function () {
-  it("Este test debe pasar siempre", function () {
-    expect(4 + 2).toBe(6);
-  });
+// describe("demo", function () {
+//   it("Este test debe pasar siempre", function () {
+//     expect(4 + 2).toBe(6);
+//   });
+// });
+
+describe("La clase Repository", () => {
+  it("espero que 2 + 6 sea igual a 8", () => {
+    expect(2 + 6).toBe(8);
+  })
 });
